bfd192e21dba6224d74fb899f104316bbfdbd328
zth <zth@zthmac.local>
2018-09-27 09:50:21 +0800

    culture venues

---
+++ csv2geojson.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from calc_path_distance import *
+import sys
+p = pd.read_csv(sys.argv[1])
+loci = list(p).index('location')
+save_pois([
+    {
+        'type':'Feature', 
+        'geometry': {'type':'Point', 'coordinates': [[float(_) for _ in r[loci].split(',')]]}, 
+        'properties': dict([(k, r[ki]) for ki, k in enumerate(p)])
+    } for r in p.values], sys.argv[2])
