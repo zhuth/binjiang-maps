38f0b216031cebdb0125785a7f484ccfc65d165a
ZTH <zthpublic@gmail.com>
2018-11-20 07:52:10 +0800

    Update address2geoloc.py

--- address2geoloc.py
+++ address2geoloc.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import amapconfig
 
 a=set()
 while True:
@@ -13,7 +14,7 @@ while True:
 a=list(a)
 for i in range(0, len(a), 10):
 	addrs = '|'.join(a[i:i+10])
-	url = 'https://restapi.amap.com/v3/geocode/geo?batch=true&key=957264d0fdf6a73904dc32db02e3999d&address=' + addrs + '&city='
+	url = 'https://restapi.amap.com/v3/geocode/geo?batch=true&key=' + amapconfig.apikey + '&address=' + addrs + '&city='
 	j=json.loads(requests.get(url).content)
 	for ad, g in zip(a[i:i+10], j['geocodes']):
 		print(ad, g['formatted_address'], g['location'])
